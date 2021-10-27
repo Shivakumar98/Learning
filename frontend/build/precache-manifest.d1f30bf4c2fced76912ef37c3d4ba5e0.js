6d7c8adbdb04a7e82d81964ae96564e80bd58e11
Shivakumar <skedlayah@skedlayah-a01.vmware.com>
2021-10-27 15:06:14 +0530

    learning and experimenting

---
+++ frontend/build/precache-manifest.d1f30bf4c2fced76912ef37c3d4ba5e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e267b6d3bdc5ac25348dea88808c579",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ba407f63c65cce6014a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e88cf915786643f1267f",
+    "url": "/static/js/2.56686953.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.56686953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ba407f63c65cce6014a",
+    "url": "/static/js/main.7c3ded04.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
